8cd1af515f397e5732a7d5616aaa5eb4a13bd2fe
Rachel <rachelmramirez@gmail.com>
2015-09-15 22:03:52 -0400

    items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
+  
   def create
+    @user = current_user
+    @item = @user.items.build(params.require(:item).permit(:name))
+      if @item.save
+        flash[:notice] = "Item saved"
+      else
+        flash[:error] = "Item not saved"
+      end
+      redirect_to user_path(@user)
+    end
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
 
   def show
+    @user = User.find(params[:id])
+    @items = @user.items
   end
 
 end
