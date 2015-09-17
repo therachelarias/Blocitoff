fa290b263305c74571c09e4b085258332a2aed54
Rachel <rachelmramirez@gmail.com>
2015-09-17 17:45:14 -0400

    updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     @items = @user.items
   end
 
